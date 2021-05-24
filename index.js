aa5c9413a115445bba204d337ba1938ed4b5bbc3
rem311621 <rem311621@yahoo.com.tw>
2021-05-24 14:42:01 +0800

    Initial commit for Heroku testing

--- index.js
+++ index.js
@@ -54,227 +54,235 @@ function handleEvent(event) {
   const echo = { type: "text", text: event.message.text };
 
   const flexmessage = {
-    type: "carousel",
-    contents: [
+    messages: [
       {
-        type: "bubble",
-        hero: {
-          type: "image",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
+        type: "flex",
+        altText: "This is a Flex Message",
+        contents: {
+          type: "carousel",
           contents: [
             {
-              type: "text",
-              text: "Arm Chair, White",
-              wrap: true,
-              weight: "bold",
-              size: "xl",
-            },
-            {
-              type: "box",
-              layout: "baseline",
-              contents: [
-                {
-                  type: "text",
-                  text: "$49",
-                  wrap: true,
-                  weight: "bold",
-                  size: "xl",
-                  flex: 0,
-                },
-                {
-                  type: "text",
-                  text: ".99",
-                  wrap: true,
-                  weight: "bold",
-                  size: "sm",
-                  flex: 0,
-                },
-              ],
-            },
-          ],
-        },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "button",
-              style: "primary",
-              action: {
-                type: "uri",
-                label: "Add to Cart",
-                uri: "https://linecorp.com",
+              type: "bubble",
+              hero: {
+                type: "image",
+                size: "full",
+                aspectRatio: "20:13",
+                aspectMode: "cover",
+                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
+              },
+              body: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Arm Chair, White",
+                    wrap: true,
+                    weight: "bold",
+                    size: "xl",
+                  },
+                  {
+                    type: "box",
+                    layout: "baseline",
+                    contents: [
+                      {
+                        type: "text",
+                        text: "$49",
+                        wrap: true,
+                        weight: "bold",
+                        size: "xl",
+                        flex: 0,
+                      },
+                      {
+                        type: "text",
+                        text: ".99",
+                        wrap: true,
+                        weight: "bold",
+                        size: "sm",
+                        flex: 0,
+                      },
+                    ],
+                  },
+                ],
+              },
+              footer: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "button",
+                    style: "primary",
+                    action: {
+                      type: "uri",
+                      label: "Add to Cart",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "Add to wishlist",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                ],
               },
             },
             {
-              type: "button",
-              action: {
-                type: "uri",
-                label: "Add to wishlist",
-                uri: "https://linecorp.com",
+              type: "bubble",
+              hero: {
+                type: "image",
+                size: "full",
+                aspectRatio: "20:13",
+                aspectMode: "cover",
+                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
               },
-            },
-          ],
-        },
-      },
-      {
-        type: "bubble",
-        hero: {
-          type: "image",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_6_carousel.png",
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "text",
-              text: "Metal Desk Lamp",
-              wrap: true,
-              weight: "bold",
-              size: "xl",
-            },
-            {
-              type: "box",
-              layout: "baseline",
-              flex: 1,
-              contents: [
-                {
-                  type: "text",
-                  text: "$11",
-                  wrap: true,
-                  weight: "bold",
-                  size: "xl",
-                  flex: 0,
-                },
-                {
-                  type: "text",
-                  text: ".99",
-                  wrap: true,
-                  weight: "bold",
-                  size: "sm",
-                  flex: 0,
-                },
-              ],
-            },
-            {
-              type: "text",
-              text: "Temporarily out of stock",
-              wrap: true,
-              size: "xxs",
-              margin: "md",
-              color: "#ff5551",
-              flex: 0,
-            },
-          ],
-        },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "button",
-              flex: 2,
-              style: "primary",
-              color: "#aaaaaa",
-              action: {
-                type: "uri",
-                label: "Add to Cart",
-                uri: "https://linecorp.com",
+              body: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Metal Desk Lamp",
+                    wrap: true,
+                    weight: "bold",
+                    size: "xl",
+                  },
+                  {
+                    type: "box",
+                    layout: "baseline",
+                    flex: 1,
+                    contents: [
+                      {
+                        type: "text",
+                        text: "$11",
+                        wrap: true,
+                        weight: "bold",
+                        size: "xl",
+                        flex: 0,
+                      },
+                      {
+                        type: "text",
+                        text: ".99",
+                        wrap: true,
+                        weight: "bold",
+                        size: "sm",
+                        flex: 0,
+                      },
+                    ],
+                  },
+                  {
+                    type: "text",
+                    text: "Temporarily out of stock",
+                    wrap: true,
+                    size: "xxs",
+                    margin: "md",
+                    color: "#ff5551",
+                    flex: 0,
+                  },
+                ],
               },
-            },
-            {
-              type: "button",
-              action: {
-                type: "uri",
-                label: "Add to wish list",
-                uri: "https://linecorp.com",
-              },
-            },
-          ],
-        },
-      },
-      {
-        type: "bubble",
-        hero: {
-          type: "image",
-          url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-          size: "full",
-          aspectRatio: "20:13",
-          aspectMode: "cover",
-        },
-        body: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "text",
-              text: "Metal Desk Lamp",
-              wrap: true,
-              weight: "bold",
-              size: "xl",
-            },
-            {
-              type: "box",
-              layout: "baseline",
-              flex: 1,
-              contents: [
-                {
-                  type: "text",
-                  text: "$11",
-                  wrap: true,
-                  weight: "bold",
-                  size: "xl",
-                  flex: 0,
-                },
-                {
-                  type: "text",
-                  text: ".99",
-                  wrap: true,
-                  weight: "bold",
-                  size: "sm",
-                  flex: 0,
-                },
-              ],
-            },
-          ],
-        },
-        footer: {
-          type: "box",
-          layout: "vertical",
-          spacing: "sm",
-          contents: [
-            {
-              type: "button",
-              flex: 2,
-              style: "primary",
-              action: {
-                type: "uri",
-                label: "Add to Cart",
-                uri: "https://linecorp.com",
+              footer: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "button",
+                    flex: 2,
+                    style: "primary",
+                    color: "#aaaaaa",
+                    action: {
+                      type: "uri",
+                      label: "Add to Cart",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "Add to wish list",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                ],
               },
             },
             {
-              type: "button",
-              action: {
-                type: "uri",
-                label: "Add to wish list",
-                uri: "https://linecorp.com",
+              type: "bubble",
+              hero: {
+                type: "image",
+                url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                size: "full",
+                aspectRatio: "20:13",
+                aspectMode: "cover",
+              },
+              body: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "text",
+                    text: "Metal Desk Lamp",
+                    wrap: true,
+                    weight: "bold",
+                    size: "xl",
+                  },
+                  {
+                    type: "box",
+                    layout: "baseline",
+                    flex: 1,
+                    contents: [
+                      {
+                        type: "text",
+                        text: "$11",
+                        wrap: true,
+                        weight: "bold",
+                        size: "xl",
+                        flex: 0,
+                      },
+                      {
+                        type: "text",
+                        text: ".99",
+                        wrap: true,
+                        weight: "bold",
+                        size: "sm",
+                        flex: 0,
+                      },
+                    ],
+                  },
+                ],
+              },
+              footer: {
+                type: "box",
+                layout: "vertical",
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "button",
+                    flex: 2,
+                    style: "primary",
+                    action: {
+                      type: "uri",
+                      label: "Add to Cart",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "Add to wish list",
+                      uri: "https://linecorp.com",
+                    },
+                  },
+                ],
               },
             },
           ],
@@ -284,7 +292,7 @@ function handleEvent(event) {
   };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  return client.replyMessage(event.replyToken, flexmessage);
 }
 
 // listen on port
